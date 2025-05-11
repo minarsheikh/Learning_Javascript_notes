6d394f785616de0f342b5545f6ebb7a7ccebea93
minarsheikh <119511790+minarsheikh@users.noreply.github.com>
2025-05-11 20:58:14 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3)  // we are using Nodejs, not browser!
+
+console.log(3
+    +
+    3
+) // Code redability should be high, dont write code like this
+
+let name = "Minar" //Make a string
+let age= 21 // Integer
+
+/*
+ #Primitive data types 
+ 1) Number -> range 2 to the power 53
+ 2) bigInt -> for too large numbers
+ 3) String => ""
+ 4) bolean => true/false
+ 5) Null => standalone value
+ 6) undefined
+ 7) symbol => unique
+
+ #Non primitive data types
+ 1)object
+ 2)Arrays
+*/
+
+console.log(typeof null); // type of null is object
+console.log(typeof undefined); // type of undefined is undefined
+
